d4db8b8dbcb41af5f19f769ba911699d1ae94fb6
Steve Storey <sstorey@gmail.com>
2017-07-24 15:21:13 +0100

    Ensure that the application starts up when deployed as a WAR in a container.

--- src/main/java/com/github/stevestorey/test/afterburnerbug/MainApplication.java
+++ src/main/java/com/github/stevestorey/test/afterburnerbug/MainApplication.java
@@ -2,6 +2,7 @@ package com.github.stevestorey.test.afterburnerbug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +13,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 
     @PostMapping("/echo")
     @ResponseBody
